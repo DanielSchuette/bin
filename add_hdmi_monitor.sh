75cf84f25979d4b73331bf2adb4b184b2e93a470
DanielSchuette <d.schuette@online.de>
2022-12-13 16:21:54 +0100

    update HDMI monitor script

--- add_hdmi_monitor.sh
+++ add_hdmi_monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+resolution='2560x1440' # discover via `xrandr'
 primary='eDP1'
 secondary='HDMI2'
-xrandr --output "$primary" --primary --output "$secondary" --mode 1920x1200 --right-of "$primary" --auto
+xrandr --output "$primary" --primary --output "$secondary" --mode "$resolution" --right-of "$primary" --auto
